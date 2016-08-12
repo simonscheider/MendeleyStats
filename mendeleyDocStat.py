9afa5b49cbdef3825bb71733a72cfa7202c1a6fe
simonscheider <simonscheider@web.de>
2016-08-12 13:45:23 +0200

    start

---
+++ mendeleyDocStat.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:       mendeleyDocStat
+# Purpose:      Creates statistics over a mendeley group" research papers
+#
+# Author:      simon
+#
+# Created:     12/08/2016
+# Copyright:   (c) simon 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+__author__      = "Simon Scheider"
+__copyright__   = ""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
